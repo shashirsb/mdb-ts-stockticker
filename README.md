d07ab93b38c6ad14fc961be004680ecc7dc7907c
Shashi Ramachandra <146806446+shashimdb@users.noreply.github.com>
2025-09-08 18:46:38 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# mdb-ts-stockticker
+1. Update MDB URL
+2. run 'python synthetic_data.py'
+3. run 'python fetch.py' 
+4. open index.html in the browser
+NOTE: You need to enable CORS Plugin in chrome browser if you run on your laptop
---
+++ fetch.py
@@ -0,0 +1,36 @@
+from flask import Flask, jsonify, send_from_directory
+from flask_cors import CORS
+from pymongo import MongoClient
+
+app = Flask(__name__, static_folder='.')
+CORS(app)
+
+client = MongoClient("mongodb+srv://mainuser:<password>@cluster0.d8jtf.mongodb.net/?retryWrites=true&w=majority")
+db = client["marketdata"]
+
+@app.get('/api/ko')
+def ko():
+    cur = db["stock_ticks_v2"].find(
+        {"ts": {"$exists": True}},
+        {"_id": 0, "ts": 1, "validStart": 1, "price_raw": 1, "price_adjusted": 1, "volume": 1, "eventTags": 1}
+    ).sort("ts", 1)
+    
+    out = [
+        {
+            "ts": doc["ts"].strftime('%Y-%m-%d'),
+            "validStart": doc["validStart"].strftime('%Y-%m-%d'),
+            "price_raw": doc["price_raw"],
+            "price_adjusted": doc["price_adjusted"],
+            "volume": doc["volume"],
+            "eventTags": doc["eventTags"]
+        }
+        for doc in cur
+    ]
+    return jsonify(out)
+
+@app.get('/')
+def index():
+    return send_from_directory('.', 'index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5001)
